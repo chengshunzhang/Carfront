91a36189541e2d0c0d92bf341c7dfab90b825ded
chengshunzhang <chengshun.zhang@outlook.com>
2018-12-31 21:05:37 -0800

    show message and confirm for delete

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {SERVER_URL} from '../constants.js'
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {confirmAlert} from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 class Carlist extends Component {
     constructor(props) {
@@ -25,8 +29,32 @@ class Carlist extends Component {
     // delete car
     onDelClick = (link) => {
         fetch(link, {method: 'DELETE'})
-        .then(res => this.fetchCars())
-        .catch(err => console.log(err));
+        .then(res => {
+            toast.success("Car deleted", {
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+            this.fetchCars()
+        })
+        .catch(err => {
+            toast.error('Error when deleting', {
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.log(err)
+        });
+    }
+
+    confirmDelete = (link) => {
+        confirmAlert({
+            message: 'Are you sure to delete?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: () => this.onDelClick(link)
+                }, {
+                    label: 'No'
+                }
+            ]
+        })
     }
 
     render() {
@@ -52,7 +80,7 @@ class Carlist extends Component {
             width: 100,
             accessor: '_links.self.href',
             Cell: ({value}) => (<button
-            onClick={() => {this.onDelClick(value)}}>
+            onClick={() => {this.confirmDelete(value)}}>
             Delete
             </button>)
         }]
@@ -60,6 +88,7 @@ class Carlist extends Component {
             <div className='App'>
                 <ReactTable data={this.state.cars} columns={columns}
                 filterable={true}/>
+                <ToastContainer autoClose={2000}/>
             </div>
         );
     }
